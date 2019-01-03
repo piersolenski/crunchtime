231cf9c43f697f4a0e84d8b95533f6701614732e
Piers Olenski <hello@piers.io>
2019-01-03 19:16:17 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tide To Crunch CSV Converter
+
+A CLI tool for converting a Tide CSV to the Crunch format
+
+## Usage
+
+### Convert a file
+
+```sh
+node index.js -i tide.csv -r 2 -o output.csv
+```
+
+### Get help
+
+```sh
+node index.js -h
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const removeEmptyLines = require('remove-blank-lines');
+const papaparse = require('papaparse');
+const dateFns = require('date-fns');
+const program = require('commander');
+const pjson = require('./package.json');
+
+program
+  .version(pjson.version)
+  .option('-i, --input <type>', 'The CSV from the Tide App')
+  .option('-r, --rows [type]', 'Rows to keep')
+  .option('-o, --output [type]', 'Name of the outputted file [marble]', 'output.csv')
+  .parse(process.argv);
+
+const FORMAT = 'DD/MM/YYYY';
+
+const currentPath = process.cwd();
+
+const convertDate = (date, format) => dateFns.format(dateFns.parse(date), format);
+
+const keepRows = (arr, n) => arr.splice(0, n);
+
+const convertToJSON = csv => papaparse.parse(removeEmptyLines(csv), { header: true, });
+
+const convertToCSV = json => papaparse.unparse(json);
+
+const crunchify = arr => arr.map(row => ({
+  Date: convertDate(row['Timestamp'], FORMAT),
+  Reference: row['Reference'],
+  Amount: row['Amount'],
+  Balance: row['Cleared balance'],
+}));
+
+fs.readFile(program.input, 'utf8', (err, data) => {
+  if (err) return console.log('File does not exist!');
+  const rows = convertToJSON(data).data;
+  const crunchifiedJSON = keepRows(crunchify(rows), program.rows || rows.length);
+  const csv = convertToCSV(crunchifiedJSON);
+
+  fs.writeFile(`${currentPath}/${program.output}`, csv, 'utf8', (err, msg) => {
+    if (err) return console.log(err);
+    console.log(`File written to ${program.output}`);
+  });
+});
